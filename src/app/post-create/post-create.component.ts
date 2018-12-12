22e4a7941b145ac38ae7b4a33c624f3dbf297703
DESKTOP-GGTATVP\thaol <quynhnga@naxam.net>
2018-12-12 21:16:47 +0700

    18. Getting user input

---
+++ src/app/post-create/post-create.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit {
+
+  enterValue = '' ;
+  newPost = 'NO CONTENT' ;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddPost() {
+  this.newPost = this.enterValue ;
+  }
+
+}
