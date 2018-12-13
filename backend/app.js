6414f4cd895c34db8d2f56f1404754ae1b8131a1
DESKTOP-GGTATVP\thaol <quynhnga@naxam.net>
2018-12-13 20:47:55 +0700

    Add server.js

---
+++ backend/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app =  express();
+app.use((req, res, next)=>{
+ console.log('First middlerware');
+ //next();
+});
+app.use((req, res, next)=>{
+  res.send('Hello from Express');
+ });
+ module.exports = app;
