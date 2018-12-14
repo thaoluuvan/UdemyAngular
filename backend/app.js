6ee206aad7dbecd3ef67948e436e5ae84d26d616
thaoluuvan <luu.van.thao@framgia.com>
2018-12-14 15:05:04 +0700

    37.Fetching Initial Posts

--- backend/app.js
+++ backend/app.js
@@ -1,47 +1,15 @@
 const express = require('express');
-<<<<<<< HEAD
 const app = express();
-app.use((req,res, next)=>{
-  console.log('This is first middleware');
-  next();
-});
-app.use((req,res, next)=>{
-  res.send('Hello from express');
+app.use('/api/posts',(req,res, next)=>{
+  const posts = [
+  {id: 'dddffaf', title: 'this is title', content: 'this is content'},
+  {id: 'dddffaf', title: 'this is title', content: 'this is content'},
+  {id: 'dddffaf', title: 'this is title', content: 'this is content'},
+  {id: 'dddffaf', title: 'this is title', content: 'this is content'},
+  ];
+  res.status(200).json({
+    message: 'Fetched data successfully',
+    posts: posts
+  });
 });
 module.exports = app;
-=======
-const app =  express();
-app.use('api/posts',(req, res, next)=>{
- console.log('First middlerware');
- next();
-});
-app.use((req, res, next)=>{
-  const posts = [
-    {
-      id: 'ddiddidi',
-      title: 'this is title',
-      content: 'this is content'
-    },
-    {
-      id: 'ddiddidi',
-      title: 'this is title',
-      content: 'this is content'
-    },
-    {
-      id: 'ddiddidi',
-      title: 'this is title',
-      content: 'this is content'
-    },
-    {
-      id: 'ddiddidi',
-      title: 'this is title',
-      content: 'this is content'
-    }
-  ]
-  res.status(200).json({
-     message: 'Fetched successfully',
-     post: posts
-  });
- });
- module.exports = app;
->>>>>>> ae4c336b235236d7fdd42b96c88611fe486ee11f
--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const app = require("./backend/app");
 const debug = require("debug")("node-angular");
 const http = require("http");
 
-<<<<<<< HEAD
-=======
-
->>>>>>> ae4c336b235236d7fdd42b96c88611fe486ee11f
 const normalizePort = val => {
   var port = parseInt(val, 10);
 
