7de0e122cafd2a44505c65608f226ee5a10dd1bc
thaoluuvan <luu.van.thao@framgia.com>
2018-12-17 14:48:47 +0700

    40. Adding the Post Backend Point

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use((req,res, next)=>{
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Header",
    "Origin,X-Request-With, Content-Type, Accept");
    res.setHeader("Access-Control-Allow-Methods","GET,POST,PATCH,DELETE,OPTIONS")
    next();
+});
+app.post('/api/posts',(req, res) => {
+  const post = req.body;
+  res.status(201);
+  console.log(post);
+  res.send.json({
+   message: 'Post added successfully!'
+  });
 });
 app.use('/api/posts',(req,res, next) => {
   const posts = [
